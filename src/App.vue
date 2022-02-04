6f76c28a065513386c46169ee491621db7990daf
Abraham Ortega <abraham111.ao@gmail.com>
2022-02-03 22:23:56 -0600

    a

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,153 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-row justify="center" align="center">
+      <v-col class="columna" cols="6">
+        <v-row justify="center" aling="center">
+          <h1>Listado de usuarios</h1>
+        </v-row>
+        <v-row justify="center">
+          <v-data-table 
+            :headers="headers"
+            :items="listadoUsuarios"
+            class = "elevation-2"
+            hide-default-footer
+            @click:row="gotoUser">
+          </v-data-table>
+        </v-row>
+      </v-col>
+      <v-col cols="6" class="columna">
+        <v-row justify="center" aling="center">
+          <user :user="itemUsuario"/>
+        </v-row>
+        <v-row justify="center" aling="center">
+          <h1>Posts</h1>
+        </v-row>
+        <v-row justify="center" aling="center">
+          <div class="scroll">
+            <v-card id="posts" color="#70e7e7" v-for="(item, index) in postsUsuario" :key="index">
+              <v-card-title>
+                  {{item.title}}
+              </v-card-title>
+              <v-card-text> 
+                {{item.body}}
+              </v-card-text>
+            </v-card>
+          </div>
+        </v-row>
+      </v-col>
+    </v-row>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import user from './components/usuario.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    user
+  },
+
+   data: () => {
+    return {
+      listadoUsuarios : [],
+      postsUsuario: null,
+      itemUsuario: null,
+      headers: [
+        {
+          text: 'Usuario',
+          align: 'center',
+          sort: 'true',
+          value: 'username'
+        },
+        {
+          text: 'Correo electronico',
+          aling: 'center',
+          sort: 'false',
+          value: 'email'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    async obtenerListado(){
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await res.json()
+        this.listadoUsuarios = data
+        //console.log(this.listadoUsuarios)
+      }
+      catch (e) {
+        console.log(e)
+      }
+    },
+//////Ir al usuario
+    gotoUser(user){
+      //console.log(user)
+      this.obtenerUser(user.id)
+      this.obtenerPosts(user.id)
+    },
+//////Obtener posts
+    async obtenerPosts (id) {
+      try{
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${id}`)
+        const data = await res.json()
+        this.postsUsuario = data
+        console.log(data)
+
+      }
+      catch (e){
+        console.log(e)
+      }
+    },
+//////Obtener usuario
+    async obtenerUser(id) {
+      try{
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        const data = await res.json()
+        this.itemUsuario = data
+        //console.log(data)
+
+      }
+      catch (e){
+        console.log(e)
+      }
+    },
+//////Random user
+    randomUser() {
+      const num = Math.round(Math.random()*10)
+      const data = {
+        id: num
+      }
+      this.gotoUser(data)
+    }
+  },
+
+  created(){
+    this.obtenerListado()
+    this.randomUser()
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#posts {
+  margin-top: 10px;
+  border-radius: 10px;
+  box-shadow: 10px 10;
+  
 }
+.scroll {
+  width: 500px;
+  overflow: auto;
+  height: 400px;
+}
+.columna{
+  margin-top: 4%;
+}
+
 </style>
---
+++ src/components/usuario.vue
@@ -0,0 +1,87 @@
+
+<template>
+
+  <div class="card">
+
+    <img class="avatar" src="https://www.pngmart.com/files/10/User-Account-PNG-Clipart.png">
+
+    <div class="infos">
+      <div class="name capitalize">
+        {{ user.username }}
+      </div>
+      <div class="birth">
+        <i class="fas fa-fw fa-user"></i>{{ user.name }}
+      </div>
+      <div class="email">
+        <i class="fas fa-fw fa-envelope"></i>{{ user.email }}
+      </div>
+      <div class="phone">
+        <i class="fas fa-fw fa-phone"></i>{{ user.phone }}
+      </div>
+      <div class="nationality capitalize">
+        <i class="fas fa-fw fa-globe-americas"></i>{{ user.address.city }}
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: ['user']
+}
+
+</script>
+
+<style scope>
+@import url("https://fonts.googleapis.com/css?family=Nunito");
+@import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
+
+
+body main .card {
+  width: 470px;
+  min-height: 185px;
+  display: flex;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background: #00ccffcc;
+}
+body main .card .avatar {
+  width: 100px;
+  height: 100px;
+  margin-right: 10px;
+  border-radius: 50%;
+}
+body main .card .infos {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+}
+body main .card .infos div:not(:last-child) {
+  margin-bottom: 10px;
+}
+body main .card .infos div.capitalize {
+  text-transform: capitalize;
+}
+body main .card .infos div.name {
+  font-size: 150%;
+  font-weight: bold;
+}
+body main .card .infos div i {
+  margin-right: 10px;
+}
+
+@media only screen and (max-width: 800px) {
+  body main {
+    flex-direction: column !important;
+    font-size: 80%;
+  }
+  body main .card {
+    max-width: 80%;
+    width: auto;
+    min-height: 150px;
+  }
+}
+</style>
